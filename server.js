72b101d29172753dcda370d1bc01c6eb65e7eafd
cxddong <cxd-dong@hotmail.com>
2025-12-09 19:37:10 -0500

    fix: use ES Module syntax for express

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
-const express = require('express');
+import express from 'express';
+
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Kidsartoon app is running on Cloud Run!');
+    res.send('Kidsartoon app is running on Cloud Run!');
 });
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
-  console.log(`Server started on port ${PORT}`);
+    console.log(`Server started on port ${PORT}`);
 });
